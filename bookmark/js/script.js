1fa14c9dafe11e52951ef6b432c2d5321e45213e
Nuku Ameyibor <nayibor@gmail.com>
2024-05-18 20:05:11 +0000

    mobile menu completed for bookmark
    mobile menu completed for bookmark project

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -1,8 +1,16 @@
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
+
 
 // tabs menu event listener
 tabs.forEach((tab) => {tab.addEventListener('click',onTabClick)})
+
+// Hamburger button listener
+btn.addEventListener('click', navToggle)
+
 
 function onTabClick(event_click){
     //Deactivate all tabs
@@ -19,5 +27,20 @@ function onTabClick(event_click){
     event_click.target.classList.add("border-softRed","border-b-4")
     const classString = event_click.target.getAttribute("data-target")
     document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove("hidden")
-    
 }
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
+}
+
+    
+    
+
